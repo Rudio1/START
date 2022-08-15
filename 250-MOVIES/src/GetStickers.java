17141405aa261c2632efaea3c7179cf5e6ffc517
Rudio1 <guilhermerudio6@gmail.com>
2022-08-15 10:20:13 -0300

    Update GetStickers

--- 250-MOVIES/src/GetStickers.java
+++ 250-MOVIES/src/GetStickers.java
@@ -1,5 +1,10 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
 
@@ -9,23 +14,36 @@ import java.awt.Graphics2D;
 public class GetStickers {
     
     public void create() throws Exception {
+        File f = new File("250-MOVIES/entrada/test.jpg");
+        String outputPath = "250-MOVIES/saida/figurinha.png";
 
-        BufferedImage image = ImageIO.read(new File("entrada/movie.jpg"));
+        //Criando nova imagem em memória
+        InputStream inputStream = new FileInputStream(f.getAbsolutePath());
+        BufferedImage image = ImageIO.read(inputStream);
         int width = image.getWidth();
         int height = image.getHeight();
         int newHeight = height + 200;
         
         BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
         
-        Graphics2D graphics = newImage.createGraphics();
-        graphics.drawImage(image, null, 0, 0);
+        Graphics2D graphics = (Graphics2D) newImage.createGraphics();
+        graphics.drawImage(image, 0, 0, null);
 
-        ImageIO.write(newImage, "png", new File("saida/figurinha.png"));
+        //Escrevendo no arquivo e alterando a cor
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setFont(fonte);
+        graphics.setColor(Color.CYAN);
+        graphics.drawString("Criando stickers", 30, newHeight - 100);
+
+        //Escrevendo a imagem em um arquivo (saida)
+        ImageIO.write(newImage, "png", new File(outputPath));
+
+        System.out.println("Sucessfull");
     }
 
     public static void main(String[] args) throws Exception {
-       var teste = new GetStickers();
-       teste.create();
+       var generate = new GetStickers();
+       generate.create();
         
     }
 }
